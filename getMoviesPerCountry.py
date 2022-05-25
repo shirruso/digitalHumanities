ea4105a5fd16cc9f551d44f6eedb49e27521a93e
shirruso <shirru@post.bgu.ac.il>
2022-05-25 23:56:38 +0300

    a

--- getMoviesPerCountry.py
+++ getMoviesPerCountry.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+keys = {'k_64p78757', 'k_ao1xicrn', 'k_cc9uodvl'}
 
 class Country:
     def __init__(self, name, shortcut, language):
@@ -9,12 +10,13 @@ class Country:
         self.language = language
 
 
-countries = [Country('Afghanistan', 'af', 'ps')
+countries = [
+             # Country('Afghanistan', 'af', 'ps')
              # Country('Egypt', 'eg', 'ar')
              # Country('Lebanon', 'lb', 'ar'),
-             #  Country('Turkey', 'tr', 'tr')
+             # Country('Turkey', 'tr', 'tr')
              # Country('Iran', 'ir', 'ar'),
-             # Country('France', 'fr', 'fr'),
+            Country('France', 'fr', 'fr')
              # Country('Germany', 'de', 'nl'),
              # Country('Italy', 'it', 'it'),
              # Country('United Kingdom', 'gb', 'en'),
@@ -56,31 +58,40 @@ def get_movies_per_country(country):
         json.dump(json_dict, outfile, indent=4)
 
 
+def get_movies_by_pages(country, years_range):
+    movies = []
+    num_of_pages = 3
+    release_data_from, release_data_to = years_range[0:4], years_range[5:]
+    for i in range(0, num_of_pages):
+        response = requests.get(
+            'https://imdb-api.com/API/AdvancedSearch/k_64p78757?release_date=%s-01-01,'
+            '%s-12-31&countries=%s&languages=%s&count=250&start=%d' % (
+                release_data_from, release_data_to, country.shortcut,
+                country.language, i * 250 + 1))
+        response_json = response.json()
+        movies_per_page = response_json['results']
+        movies.append(movies_per_page)
+    flat_movies = [movie for sublist in movies for movie in sublist]
+    return flat_movies
+
+
 # The function returns an array of movies that have been released over the years. The array contains for each movie
 # its id as well as its title.
 
 
 def get_movies_per_years(country, years_range):
-    release_data_from, release_data_to = years_range[0:4], years_range[5:]
-    response = requests.get(
-        'https://imdb-api.com/API/AdvancedSearch/k_ao1xicrn?release_date=%s-01-01,'
-        '%s-12-31&countries=%s&languages=%s&count=250' % (release_data_from, release_data_to, country.shortcut,
-                                                          country.language))
-    response_json = response.json()
-    movies = response_json['results']
+    movies = get_movies_by_pages(country, years_range)
     filtered_movies = []
     for movie in movies:
-        response = requests.get('https://imdb-api.com/en/API/FullCast/k_ao1xicrn/%s' % movie['id'])
-        response_json = response.json();
-        main_character = response_json['actors'][0]
-        print( main_character['id'])
-        response = requests.get('https://api.themoviedb.org/3/find/%s?api_key=dafbfad599cde5d98a5d1c68cef38b1c&external_source=imdb_id' % main_character['id'])
+        response = requests.get('https://api.themoviedb.org/3/movie/%s/credits?api_key=dafbfad599cde5d98a5d1c68cef38b1c'
+                                % movie['id'])
         response_json = response.json()
-        main_character_details = response_json['person_results']
-        print(main_character_details[0]['name'])
+        if (not response.ok) or len(response_json['cast']) == 0:
+            continue
+        main_character = response_json['cast'][0]
         data = {'id': movie['id'],
                 'title': movie['title'],
-                'main_character_name': main_character_details[0]['name'],
-                'main_character_gender': main_character_details[0]['gender']}
+                'main_character_name': main_character['name'],
+                'main_character_gender': main_character['gender']}
         filtered_movies.append(data)
     return filtered_movies
